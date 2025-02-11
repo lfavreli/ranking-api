c83886e779f57e5c114e51f1e96316c1b3a246e8
Loïc FAVRELIERE <17621704+lfavreli@users.noreply.github.com>
2025-02-11 02:55:33 +0100

    feat: add test for root path

---
+++ src/test/kotlin/fr/lfavreli/ranking/ApplicationTest.kt
@@ -0,0 +1,24 @@
+package fr.lfavreli.ranking.fr.lfavreli.ranking
+
+import fr.lfavreli.ranking.configureFrameworks
+import fr.lfavreli.ranking.configureRouting
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun `test root path is UP`() = testApplication {
+        application {
+            configureFrameworks()
+            configureRouting()
+        }
+        val response = client.get("/")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("UP", response.bodyAsText())
+    }
+}
